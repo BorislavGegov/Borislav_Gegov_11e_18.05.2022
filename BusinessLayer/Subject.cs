e48cd3cba2f0e46635657247935e4c6d531b9df7
Borislav <TTEgaming@gmail.com>
2022-05-18 16:09:12 +0300

    Add project files.

---
+++ BusinessLayer/Subject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{
+    [Serializable]
+    public class Subject
+    {
+        public string ID { get; private set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public List<Teacher> Teachers { get; set; }
+        private Subject()
+        { }
+
+        public Subject(string title, string type)
+        {
+            ID = Guid.NewGuid().ToString();
+            Title = title;
+            Type = type;
+        }
+    }
+}
---
+++ BusinessLayer/Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BusinessLayer
+{
+    [Serializable]
+    public class Teacher
+    {
+        
+        public string ID { get; private set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Experience { get; set; }
+        public List<Subject> Subjects { get; set; }
+        private Teacher()
+        { }
+
+        public Teacher(string name, int age,string experience)
+        {
+            this.ID = Guid.NewGuid().ToString();
+            Name = name;
+            Age = age;
+            Experience = experience;
+        }
+    }
+}
